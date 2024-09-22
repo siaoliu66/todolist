5353a874e0bfedb86caf0876beef930e0f93835c
siaoliu66 <siaoliu66@gmail.com>
2024-09-23 01:56:26 +0800

    新增拖曳功能

--- todolist.js
+++ todolist.js
@@ -13,6 +13,7 @@ var app = new Vue({
     catchtitle:'', 
     time: today,
     storageArray:JSON.parse(localStorage.getItem('stared')) || [],
+
   },
   methods:{
     addTodo:function(){
@@ -73,7 +74,6 @@ localStorage.setItem('stared',JSON.stringify(this.storageArray))
           }
         })
          return newtodos
-        
       }
       else{
         var newtodos=[]
@@ -87,6 +87,11 @@ localStorage.setItem('stared',JSON.stringify(this.storageArray))
     },
     undoneTodos: function(){
       return this.storageArray.filter(todo =>todo.completed != true);
+    },
+    barWidth: function(){
+      var untodo = this.storageArray.filter(todo =>todo.completed != true)
+      var todo = this.storageArray
+      return (untodo.length) / (todo.length)
     }
   },
   mounted:function(){
@@ -96,6 +101,7 @@ localStorage.setItem('stared',JSON.stringify(this.storageArray))
     }else{
         this.storageArray = JSON.parse(localStorage.getItem('stared'))
     }
+    console.log(this.filitertodos.length)
   },
   watch:{
     /*對name值進行監聽，只要name改變了，就會觸發程式
@@ -106,26 +112,11 @@ localStorage.setItem('stared',JSON.stringify(this.storageArray))
       },
       deep:true
     },
+
   },
 })
 
-$(".toggle").click(function() {
-   if($(".toggle").prop("checked")) {
-     $('body').addClass('black')
-  //    $('.top').append(`<div class="user">
-  //   <h1>
-  //     尋找快樂的人♥
-  //   </h1>
-  // </div>
-  // <div class="today">
-  //   <p> ${today} </p>
-  //   <p>喜歡笑的人運氣通常不會太差♥</p>
-  // </div>`)
-   } else {
-     $('body').removeClass('black') 
-     $('.top').children().remove()
-   }
-});
+
 
 //倒數
 // var timer;     
@@ -147,8 +138,11 @@ $(".toggle").click(function() {
 //     timer = setInterval("CountDown()",1000);      
 // });  
 
-
-console.log("不管之後會遇到多少奇怪的事情，我都會想陪著你走完")
-console.log("木頭直男 學姊狗狗 哥哥 都是學姊限定版")
-console.log("不用擔心我會對別人開特例 因為我雙眼都只有妳 沒有別人")
+console.log("雖然最近還是會有點煎熬，但我還是會想陪著你")
+console.log("可能還是有些還沒有解開的情緒，覺得很難接受也沒關係，我還是會努力讓你可以好好放下這些情緒，但就慢慢來沒關係的")
+console.log("然後學姊對不起，明明家應該是一個很放心的地方，不是像現在一樣讓你覺得有些陰影，所以就還是照你想做的來，但學妹也還是想了一些替代方案給你參考。")
+console.log("希望你不要覺得有壓力，喜歡不喜歡都可以直接告訴我，希望你不是像他說的一樣因為不敢拒絕，才都一直勉強自己跟我見面或出去。")
+console.log("也希望之後狀況都可以慢慢的轉好，不管是搬家後又或是接下來我們各自的努力，都還是希望學姊可以輕鬆一點，不用擔心也不用再害怕惹。")
+console.log("木頭直男、學姊狗狗、哥哥 都是學姊限定版♡♡♡")
+console.log("不用擔心我會對別人開特例，因為我雙眼都只有妳，沒有別人。")
 console.log("就算有人會冒出來打擾，妳永遠是學妹拒絕別人的唯一理由。")
